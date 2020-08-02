321fddc789e40f8245987b1853d9e80cb709f2d7
Michael Graham <michael@forested.earth>
2020-08-01 17:32:02 -0700

    Simplify test suite results for passing tests

--- exifgc.test.js
+++ exifgc.test.js
@@ -1,20 +1,21 @@
 const assert = require('assert')
 const exifgcPath = require('./exifgc').exifgcPath
 
-exifgcPath('./fixtures/giant-sequoia-copenhagen-botanical-garden-no-gps.jpg')
-  .then((result) => {
-    console.log('got no gps result:')
-    console.log(result)
-    assert(!result)
-  })
-
-  .catch((error) => {
-    throw error
-  })
-
-exifgcPath('./fixtures/quarry-lakes-with-gps.jpg')
-  .then((result) => {
-    console.log('got gps result')
-    console.log(result)
-    assert(result)
-  })
+  exifgcPath('./fixtures/giant-sequoia-copenhagen-botanical-garden-no-gps.jpg')
+    .then((result) => {
+      // no-gps image should return false
+      assert(!result)
+      return exifgcPath('./fixtures/quarry-lakes-with-gps.jpg')
+    })
+    .then((result) => {
+      // image with gps should return coordinates
+      assert(result)
+      assert(result.latitude)
+      assert(result.longitude)
+      console.log('SUCCESS')
+    })
+    .catch((error) => {
+      console.log('test failed:')
+      console.log(error)
+      throw error
+    })
